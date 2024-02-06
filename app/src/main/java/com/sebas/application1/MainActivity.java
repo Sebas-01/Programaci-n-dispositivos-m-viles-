3b61322e1fc8f90926471a240c630b0938a004dd
Sebas-01 <128066214+Sebas-01@users.noreply.github.com>
2024-02-06 09:11:01 -0500

    creacion de variable
    creacion de variables proyecto

--- app/src/main/java/com/sebas/application1/MainActivity.java
+++ app/src/main/java/com/sebas/application1/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean result = true;
+    String name = "Perez";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
